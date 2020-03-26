785f9638957c82bbda57a0c02938a1ff6b3babea
cristianosantosoliveira <cristiano.santos.oliveira@outlook.com>
2020-03-25 23:29:50 -0300

    initial commit

---
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/* main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		/* Escreve texto no console*/
+		System.out.println("Programa em Java");
+		
+	}
+}
