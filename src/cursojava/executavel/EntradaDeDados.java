88da981dd4fffedf79de00c79407c53fe5750a5b
cristianosantosoliveira <cristiano.santos.oliveira@outlook.com>
2020-03-29 15:16:49 -0300

    Entrada de Dados em Java com JOptionPane

---
+++ src/cursojava/executavel/EntradaDeDados.java
@@ -0,0 +1,25 @@
+package cursojava.executavel;
+
+import javax.swing.JOptionPane;
+
+public class EntradaDeDados {
+
+	public static void main(String[] args) {
+		// Entrada de Dados em Java
+
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carro? ");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas? ");
+
+		double carroNumero = Double.parseDouble(carros);
+		double pessoaNumero = Double.parseDouble(pessoas);
+
+		int divisao = (int) (carroNumero / pessoaNumero);
+
+		double resto = carroNumero % pessoaNumero;
+
+		JOptionPane.showMessageDialog(null,
+				"Divisão para pessoa deu " + divisao + " carros e sobrou " + resto + " carros");
+
+	}
+
+}
