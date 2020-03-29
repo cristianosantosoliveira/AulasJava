138ae5c8cf3bf83bdbb35564cf5db01af2a15749
cristianosantosoliveira <cristiano.santos.oliveira@outlook.com>
2020-03-29 15:01:11 -0300

    Modulo de divisão em Java

---
+++ src/cursojava/executavel/OperadorModulo.java
@@ -0,0 +1,18 @@
+package cursojava.executavel;
+
+public class OperadorModulo {
+
+	public static void main(String[] args) {
+		//Modulo: Resto da divisão %
+		
+		double carro = 9;
+		double pessoas = 2;
+		
+		double resto = carro % pessoas;
+		
+		System.out.println("Sobraram (= modulo de divisão) exatamente " + resto + " carro de " + carro + " " +
+		" dividido por " + pessoas + " pessoas ");
+
+	}
+
+}
